a676b3458c8e2be234502937c91d7e4925ee7731
jmartin <jmartin@zikzakmedia.com>
2015-04-08 10:42:13 +0200

    Add create method in order to set the code of the variant equals to the base_code of the template in case code of variant was not provided

--- product.py
+++ product.py
@@ -11,3 +11,16 @@ __metaclass__ = PoolMeta
 class Template:
     __name__ = "product.template"
     base_code = fields.Char('Base Code', help='Base Code Product Template')
+
+    @classmethod
+    def create(cls, vlist):
+        for vals in vlist:
+            if 'base_code' in vals:
+                if 'products' in vals:
+                    product = vals['products'][0]
+                    if product[0] == 'create':
+                        for prod_vals in product[1]:
+                            if 'code' in prod_vals and not prod_vals['code']:
+                                prod_vals['code'] = vals['base_code']
+                                break
+        return super(Template, cls).create(vlist)
